fb4138e7033636e4e9283e19544bb2673d59bbe2
vibharajan <vibharajan92@gmail.com>
2016-01-03 20:02:59 +0530

    Update titanic.R

--- titanic.R
+++ titanic.R
@@ -2,4 +2,4 @@ setwd("~/Kaggle/Titanic/Data")
 train<-read.csv("~/Kaggle/Titanic/Data/train.csv")
 test<-read.csv("~/Kaggle/Titanic/Data/test.csv")
 test$Survived<-rep(0,416)
-
+okay so this is totally a trial
